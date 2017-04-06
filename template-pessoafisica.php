1f01268e77a2ba38cf3a664a5ec981a764887224
Diego Rojas <diego@brasa.art.br>
2017-04-05 21:00:38 -0300

    adicionado screenshot, css que estava no customizer, retirada classe metabox, adicionada sidebar e template para pagina pessoa fisica

---
+++ template-pessoafisica.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ *Template Name: Pessoa Fisica
+ *
+ * @package OnePress
+ */
+
+get_header(); ?>
+
+	<div id="content" class="site-content">
+		<div class="page-header">
+			<div class="container">
+				<header class="entry-header">
+					<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+			</div>
+		</div>
+		<?php echo onepress_breadcrumb(); ?>
+		<div id="content-inside" class="container right-sidebar">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+						?>
+
+					<?php endwhile; // End of the loop. ?>
+
+				</main><!-- #main -->
+			</div><!-- #primary -->
+
+			<?php get_sidebar('pessoafisica'); ?>
+
+		</div><!--#content-inside -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
